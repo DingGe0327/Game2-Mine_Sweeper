f1200adf86b7acc158f66f8fbe363fa0db1dc4c9
rare200327 <435897971@qq.com>
2023-05-08 09:55:25 +0800

    关于游戏相关的函数声明，符号声明，头文件的包含

--- game.h
+++ game.h
@@ -1,7 +1,21 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 //关于游戏相关的函数声明，符号声明，头文件的包含
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
+#define ROW 9
+#define COL 9
+
+#define ROWS ROW+2
+#define COLS COL+2
+
+#define EASY_COUNT 10
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set);
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void SetMine(char board[ROWS][COLS], int row, int col);
+void FindMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
 
 
 
