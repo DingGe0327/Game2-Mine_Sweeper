0b9fcf1b1eeccf280ab1260f0f781a8a9b02e1fe
rare200327 <435897971@qq.com>
2023-05-08 09:54:39 +0800

    扫雷游戏的主函数

--- test.c
+++ test.c
@@ -8,10 +8,31 @@ void menu()
 	printf("               0.退出游戏                    \n");
 	printf("                                             \n");
 }
+void game()
+{
+	char mine[ROWS][COLS] = { 0 };//存放布置好的雷的信息
+	char show[ROWS][COLS] = { 0 };//存放排查出的雷的信息
+	//初始化数组的内容为指定的内容
+	//mine 数组在没有布置雷的时候，都是'0'
+	InitBoard(mine, ROWS, COLS, '0');
+	//show 数组在没有排查雷的时候，都是'*'
+	InitBoard(show, ROWS, COLS, '*');
+
+	//设置雷
+	SetMine(mine, ROW, COL);
+
+	DisplayBoard(show, ROW, COL);
+	//DisplayBoard(mine, ROW, COL);
+
+	//排查雷
+	FindMine(mine, show, ROW, COL);
+}
+
 int main()
 {
-
 	int input = 0;
+	//设置随机数的生成起点
+	srand((unsigned int)time(NULL));
 	do
 	{
 	    menu();
